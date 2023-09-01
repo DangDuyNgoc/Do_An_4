5cd3fc0bc81fe90c42ed3ee619b194e66fa4583d
hackTran <ductv.21it@vku.udn.vn>
2023-09-01 15:50:37 +0700

    server

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+import authRoutes from "./routes/authRoute.js";
+import connectDB from "./config/db.js";
+
+// config env
+dotenv.config();
+
+// database config
+connectDB();
+
+// rest object
+const app = express();
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// routes
+app.use("/api/v1/auth", authRoutes, bodyParser.json());
+
+
+// middleware 
+app.use(express.json());
+app.use(morgan('dev'));
+
+// rest api
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Ecommerce app</h1>');
+})
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// run listen
+app.listen(PORT, ()=> {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
